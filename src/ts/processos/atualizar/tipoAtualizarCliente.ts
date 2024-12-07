602ab5d1f265bc4bbfcc12acd792822f8466dbfb
Pastel Caique <caiquepericles.23@gmail.com>
2024-12-06 22:04:48 -0300

    Edição de dependentes

--- src/ts/processos/atualizar/tipoAtualizarCliente.ts
+++ src/ts/processos/atualizar/tipoAtualizarCliente.ts
@@ -1,5 +1,8 @@
 import Processo from "../../abstracoes/processo";
+import Armazem from "../../dominio/armazem";
 import MenuTipoAtualizarCliente from "../../menus/menuTipoAtualizarCliente";
+import Cliente from "../../modelos/cliente";
+import AtualizarClienteDependente from "./atualizarClienteDependente";
 import AtualizarClienteTitular from "./atualizarClienteTitular";
 
 export default class TipoAtualizarCliente extends Processo {
@@ -18,6 +21,27 @@ export default class TipoAtualizarCliente extends Processo {
                 this.processo = new AtualizarClienteTitular()
                 this.processo.processar()
                 break
+            
+            case 2:
+                let clienteTitular: Cliente
+                while (true) {
+                    let nomeTitular = this.entrada.receberTexto(`Digite o nome do cliente titular (0 para sair): `)
+                    if (nomeTitular === "0") {
+                        return
+                    }
+
+                    let clienteTitularTemp = Armazem.InstanciaUnica.Clientes.find(c => c.Nome.toLowerCase() === nomeTitular.toLowerCase())
+                    if (clienteTitularTemp) {
+                        clienteTitular = clienteTitularTemp
+                        break
+                    } else {
+                        console.log("Não existe cliente titular com esse nome!");
+                    }
+                }
+                this.processo = new AtualizarClienteDependente(clienteTitular.Dependentes)
+                this.processo.processar()
+                break
+
             default:
                 console.log('Opção não entendida >:(')
         }
